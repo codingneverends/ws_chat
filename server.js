d9da4c3cf6df481eae036b9672d2ce90370a10a4
codingneverends <gaganlal7171@gmail.com>
2021-03-24 20:15:47 +0530

    g

--- server.js
+++ server.js
@@ -11,13 +11,16 @@ var server = http.createServer(function(request, response) {
 server.listen(port, function() {
     console.log((new Date()) + ' Server is listening on port 3002');
 });
+//Store no of connected users
 var users=0;
 wsServer = new WebSocketServer({
     httpServer: server,
     autoAcceptConnections: false
 });
 
+// chats updated by clients will be stored in this variable as collection of jsons
 var chats=[{name : "Haya",msg:"Hello",time:1616584395815}];
+//last update details will be stored here
 var last_updates={};
 
 function originIsAllowed(origin) {
@@ -29,45 +32,49 @@ function originIsAllowed(origin) {
 
 wsServer.on('request',async (request)=> {
     if (!originIsAllowed(request.origin)) {
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
+        //Rejection of connection from other domains
+        request.reject();
+        console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+        return;
     }
     var connection = request.accept('echo-protocol', request.origin);
+    //Each user connect sucessfully
     users++;
-    last_updates["users"]=new Date().getTime();
+    last_updates["users"]=new Date().getTime();//One use connected so last update is modified
     console.log((new Date()) + ' Connection accepted.');
 
     connection.on('message', async function(message) {
         if (message.type === 'utf8') {
             var _data=JSON.parse(message.utf8Data);
-            var regex=/^[a-zA-Z0-9]+(?:_[A-Za-z0-9]+)*$/;
+            //whateever user had asked that itself initiated as response
             var response=_data;
             response["err"]=true;
             response["data"]=null;
-            var update__=true;
-            if(_data.updateto==undefined || _data.updateto==null || _data.updateto=="" || !_data.updateto.match(regex)){
-                update__=false;
-            }
             if(_data.action=="_updates"){
+                //when ever user call request for updates last_updtaes is returned
                 response["data"]=last_updates;
                 response["err"]=false;
                 connection.sendUTF(JSON.stringify(response));
             }
             else{
                 if(_data.action=="add"){
+                    //Adds data to chat 
                     chats.push(_data.json);
+                    //Modifying last update
                     last_updates["chat_up"]=new Date().getTime();
+                    //Chat length exceeds 50 will remove first chat
                     if(chats.length>50)
                         chats.shift();
                 }
                 else if(_data.action=="get"){
+                    //Retuns chats
                     response["data"]=chats;
                     response["err"]=false;
                     connection.sendUTF(JSON.stringify(response));
                     
                 }
                 else if(_data.description=="getusers"){
+                    //Reurn number of users connected to web socket
                     response["data"]=users;
                     response["err"]=false;
                     connection.sendUTF(JSON.stringify(response));
@@ -75,18 +82,21 @@ wsServer.on('request',async (request)=> {
                 else if(_data.action=="delete"){
                     temp=[];
                     var deleted=false;
+                    //Delete is invoked by time(TimeStamp) of chat inserted
                     for(i in chats){
                         if(chats[i].time==_data.json){
                             deleted=chats[i];
+                            //Deletion also invokes changes in chats
+                            last_updates["chat_up"]=new Date().getTime();
                         }
                         else{
+                            //Other value is pushed to temp
                             temp.push(chats[i]);
                         }
                     }
                     chats=temp;
-                    if(deleted){
-                        last_updates["chat_up"]=new Date().getTime();
-                    }
+                    response["data"]=deleted;
+                    connection.sendUTF(JSON.stringify(response));
                 }
                 else{
                     response["error"]="Action is not resolved";
